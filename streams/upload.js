8b3a3882b0d1546f8c19259c68f19e9af11ada49
Artur Ceschin <artur.ceschin@gmail.com>
2023-11-21 15:33:19 -0300

    refactor: small improvements in the uplaod stream

--- streams/upload.js
+++ streams/upload.js
@@ -1,34 +1,47 @@
 import { parse } from 'csv-parse';
 import fs from 'node:fs';
 
-const csvPath = new URL('./tasks.csv', import.meta.url);
+const CSV_FILE_PATH = new URL('./tasks.csv', import.meta.url);
+const SERVER_URL = 'http://localhost:4000/tasks';
 
-const stream = fs.createReadStream(csvPath);
+async function readAndUploadCSV() {
+  try {
+    const csvStream = fs.createReadStream(CSV_FILE_PATH, { encoding: 'utf8' });
 
-const csvParse = parse({
-  delimiter: ';',
-  skipEmptyLines: true,
-  fromLine: 2
-});
+    const csvParser = parse({
+      delimiter: ';',
+      skipEmptyLines: true,
+      fromLine: 2
+    });
 
-async function upload() {
-  const linesParse = stream.pipe(csvParse);
+    const linesParse = csvStream.pipe(csvParser);
 
-  for await (const line of linesParse) {
-    const [title, description] = line;
+    for await (const line of linesParse) {
+      const [title, description] = line;
 
-    await fetch('http://localhost:4000/tasks', {
+      await postDataToServer({ title, description });
+    }
+
+    console.log('CSV upload completed.');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function postDataToServer(data) {
+  try {
+    await fetch(SERVER_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        title,
-        description,
-      })
-    })
-  }
+      body: JSON.stringify(data),
+    });
 
+    console.log('Data uploaded successfully.');
+  } catch (error) {
+    console.error('Error posting data:', error);
+  }
 }
 
-upload()
+readAndUploadCSV();
